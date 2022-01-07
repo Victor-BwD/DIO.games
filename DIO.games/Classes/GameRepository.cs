c984d86f7b6602a68780a9918a82986bf7af3d6c
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-01-07 17:07:26 -0300

    [UPDATED] Added new interface and new class GameRepository

---
+++ DIO.games/Classes/GameRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DIO.games.Interfaces;
+
+namespace DIO.games
+{
+    class GameRepository : iRepository<Games> // it will replace the T in iRepository with Games.
+    {
+        private List<Games> listGames = new List<Games>();
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Games entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Games> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NextId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Games ReturnById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Games entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DIO.games/Interfaces/iRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.games.Interfaces
+{
+    interface iRepository<T>
+    {
+        List<T> Lista();
+
+        T ReturnById(int id);
+
+        void Insert(T entity);
+
+        void Delete(int id);
+
+        void Update(int id, T entity);
+
+        int NextId();
+    }
+}
