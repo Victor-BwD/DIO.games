440b46da7c4d36c87cb36998ae6d6391a0248619
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-01-07 16:48:51 -0300

    [ADDED] Added interfaces, classes and enum folder and entitybase and games classes and enum genre

---
+++ DIO.games/Classes/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.games
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; protected set; }
+    }
+}
--- DIO.games/Program.cs
+++ DIO.games/Program.cs
@@ -6,6 +6,7 @@ namespace DIO.games
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
