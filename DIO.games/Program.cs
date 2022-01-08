dab14657451518c9b1dcca3764ec604af3fd3779
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-01-08 01:07:44 -0300

    [ADDED] Added insert new game logic in the list

--- DIO.games/Program.cs
+++ DIO.games/Program.cs
@@ -58,7 +58,36 @@ namespace DIO.games
                 Console.WriteLine("#ID: {0}: - {1}", game.ReturnId(), game.ReturnTitle());
             }
         }
+        
+        private static void InsertGame()
+        {
+            Console.WriteLine("Insert new game");
 
+            foreach (int i in Enum.GetValues(typeof(Genre)))
+            {
+                Console.WriteLine("{0}.{1}", i, Enum.GetName(typeof(Genre), i));
+            }
+
+            Console.Write("Type the genre between options above: ");
+            int inputGenre = int.Parse(Console.ReadLine());
+
+            Console.Write("Type the game title: ");
+            string inputTitle = Console.ReadLine();
+
+            Console.Write("Type the game year: ");
+            int inputYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Type the game description: ");
+            string inputDescription = Console.ReadLine();
+
+            Games newGame = new Games(id: repository.NextId(),
+                                      genre: (Genre)inputGenre,
+                                      title: inputTitle,
+                                      year: inputYear,
+                                      description: inputDescription);
+
+            repository.Insert(newGame);
+        }
 
         private static string GetUserOption()
         {
